4834328ee7dc46203e68355a91f9736f123045cf
maryori <mary.gutierrez@duocuc.cl>
2026-02-03 22:12:17 -0300

    some

--- src/services/integrations.js
+++ src/services/integrations.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000/api";
+//const API_URL = "http://localhost:3000/api";
+const API_URL = "https://backend-techstore-1-b4sj.onrender.com";
 
 export const Integrations = {
   Products: {
